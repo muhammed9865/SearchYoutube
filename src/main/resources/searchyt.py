c3d03e4897ac6998e984fd2e3ecd872285c13742
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-11-29 16:33:04 +0200

    new: enable setting max videos number per request

--- src/main/resources/searchyt.py
+++ src/main/resources/searchyt.py
@@ -1,7 +1,7 @@
 from youtube_search import YoutubeSearch
 import sys
-def search(query: str):
-    results = YoutubeSearch(query, max_results=10).to_json()
+def search(query: str, max_result: int):
+    results = YoutubeSearch(query, max_results=max_result).to_json()
 
 
     file = open("result.json", "w")
@@ -12,4 +12,9 @@ def search(query: str):
 
 
 if __name__ == "__main__":
-    search(sys.argv[1])
+    max_res = 10
+    if len(sys.argv) > 2:
+        max_res = int(sys.argv[2])
+        print(max_res)
+
+    search(sys.argv[1], max_res)
